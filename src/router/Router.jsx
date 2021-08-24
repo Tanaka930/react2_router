663de75adbce686dc22250d9dae7029f8c46fe56
Tanaka930 <55372083+Tanaka930@users.noreply.github.com>
2021-08-24 18:12:02 +0900

    404

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,16 @@
 import { Switch, Route } from "react-router-dom";
 
+import { Home } from "../Home";
 import { page1Routes } from "./Page1Routes";
 import { page2Routes } from "./Page2Routes";
+import { Page404 } from "../Page404";
 
 export const Router = () => {
   return (
     <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
       <Route
         path="/page1"
         render={({ match: { url } }) => (
@@ -38,6 +43,9 @@ export const Router = () => {
           </Switch>
         )}
       />
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
   );
 };
